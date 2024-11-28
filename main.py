77dfca2331d68689cb1665c500ccd53f8b376f26
Dorian Kita <dorian.kita90@gmail.com>
2024-11-28 18:38:26 +0100

    added colorfull turtles

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ from turtle import Turtle,Screen
 screen = Screen()
 screen.setup(width=500,height=400)
 user_bet = screen.textinput(title='Make bet', prompt="Which turtle will win the race? Enter a color: ")
+colors = ['red','orange','yellow','green','blue','purple']
+y = -120
 
-tim = Turtle()
-
+for color in colors:
+    tim = Turtle()
+    tim.shape('turtle')
+    tim.color(color)
+    tim.penup()
+    tim.goto(-230,y)
+    y += 50
 
 
 screen.exitonclick()
